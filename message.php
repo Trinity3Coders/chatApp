74e9f8db227d7e4960330806cb968a69bd42bf05
TrinityCoders <trinity3coders@gmail.com>
2020-02-29 04:31:19 +0530

    i

--- message.php
+++ message.php
@@ -1,7 +1,16 @@
 <?php 
 
 	include('connect.php');
+<<<<<<< HEAD
+	session_start();
+	$message = $_POST['usermsg'];
+	$name = $_SESSION['username'];
+	print $name;
+
+ ?>
+=======
 	
 	$message = $_POST['usermsg'];
 	print $message;
  ?>
+>>>>>>> 451bac66432a1107b6a20485d49d7fa2beb69d1f
